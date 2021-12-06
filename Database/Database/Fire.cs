36a27749742dd5e2aad1d2877adfb48fbb6e2f85
Bas <30810@ma-web.nl>
2021-12-06 15:04:58 +0100

    pokedex is nu interactief

---
+++ Database/Database/Fire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    class Fire : Pokemon
+    {
+        private string type = "Fire";
+        public Fire(string name, string aType) : base(name, aType)
+        {
+            type = aType;
+        }
+
+        public override string GetType()
+        {
+            return type;
+        }
+    }
+}
--- Database/Database/Program.cs
+++ Database/Database/Program.cs
@@ -10,15 +10,36 @@ namespace Database
         {
             Pokedex dex = new Pokedex();
 
-            Pokemon aPokemon = new Normale("Meowth", "Normale");
+            //alle pokemons adden voor de dex
 
-            dex.AddPokemon(aPokemon);
+            Pokemon meowth = new Normale("Meowth", "Normale");
+            dex.AddPokemon(meowth);
 
-            foreach (Pokemon p in dex.GetPokemons())
+            Pokemon snorlax = new Normale("Snorlax", "Normale");
+            dex.AddPokemon(snorlax);
+
+            Pokemon charmander = new Fire("Charmander", "Fire");
+            dex.AddPokemon(charmander);
+
+            Pokemon litwick = new Fire("Litwick", "Fire");
+            dex.AddPokemon(litwick);
+
+            while (true)
             {
-                Console.WriteLine(p.GetName());
-                Console.WriteLine(p.GetType());
-            }
+                string zoek;
+                Console.WriteLine("\nZoek een Pokémon op! (Met hoofdletter)");
+                zoek = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                foreach (Pokemon p in dex.GetPokemons())
+                {
+                    if (zoek == p.GetName())
+                    {
+                        Console.WriteLine(p.GetName());
+                        Console.WriteLine(p.GetType());
+                    }
+                }
+            }           
         }
     }
 }
