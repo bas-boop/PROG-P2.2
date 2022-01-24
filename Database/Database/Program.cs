eb40e4c84ee6b2369e05f7c5a9aa9884bc70f9ca
Bas <30810@ma-web.nl>
2022-01-24 14:56:56 +0100

    Je kan niet pokemons zelf toevoegen

--- Database/Database/Program.cs
+++ Database/Database/Program.cs
@@ -6,6 +6,10 @@ namespace Database
 {
     class Program
     {
+        /// <summary>
+        /// Mijn hele mooie Pokédex waar je meerde Pokémons in kan doen en kan vinden via hun naam types of evulotion state.
+        /// </summary>
+
         static void Main()
         {
             Pokedex dex = new Pokedex();
@@ -26,29 +30,54 @@ namespace Database
 
             while (true)//Dit is Zoek() denk ik
             {
-                string zoek;
-                Console.WriteLine("\nJe kan Pokémons zoek met een hun naam, met hun type, of via hun evolotion state(First, Middle, Last)(vergeet geen Hoofdletter).");
-                zoek = Console.ReadLine();
-                Console.WriteLine("\n");
-
-                foreach (Pokemon p in dex.GetPokemons())
+                Console.WriteLine("\nWil je een Pokémon toevoegen y/n? zo nee, dan ga je zoeken.");
+                string vraag = Console.ReadLine();
+                if (vraag == "y")
                 {
-                    if (zoek == p.GetName())
+                    string name, t1, t2, es;
+                    Console.WriteLine("\nWat is de naam van jouw Pokémon?");
+                    name = Console.ReadLine();
+                    Console.WriteLine("\nEerste type?");
+                    t1 = Console.ReadLine();
+                    Console.WriteLine("\nTweede type?");
+                    t2 = Console.ReadLine();
+                    Console.WriteLine("\nEvelution state?");
+                    es = Console.ReadLine();
+                    dex.AddPokemon(new Pokemon(name, t1, t2, es));
+                }
+                else if(vraag == "n")
+                {
+                    Console.WriteLine("\nJe kan Pokémons zoek met een hun naam, met hun type, of via hun evolotion state(First, Middle, Last)(vergeet geen Hoofdletter).");
+                    string zoek = Console.ReadLine();
+                    Console.WriteLine("\n");
+
+                    foreach (Pokemon p in dex.GetPokemons())
                     {
-                        Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState());
+                        if (zoek == p.GetName())
+                        {
+                            Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
+                        }
+                        if (zoek == p.GetType())
+                        {
+                            Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
+                        }
+                        if (zoek == p.GetType2())
+                        {
+                            Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
+                        }
+                        if (zoek == p.GetEvolotionState())
+                        {
+                            Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
+                        }
                     }
-                    if (zoek == p.GetType())
-                    {
-                        Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
-                    }
-                    if (zoek == p.GetType2())
-                    {
-                        Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
-                    }
-                    if (zoek == p.GetEvolotionState())
-                    {
-                        Console.WriteLine(p.GetName() + "\n" + p.GetType() + " " + p.GetType2() + "\nDe evolotion state is " + p.GetEvolotionState() + "\n");
-                    }
+                }
+                else if(vraag == "F")
+                {
+                    Console.WriteLine("\n\nF\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("\n\nEY, je mag alleen y of n zeggen\n\n");
                 }
             }           
         }
